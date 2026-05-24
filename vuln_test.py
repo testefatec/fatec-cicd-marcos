6a9ac1f696ca6bc2df6d2826f7816d69e7c7ebcf
Idirley <idirley.soares@fatec.sp.gov.br>
2026-05-24 20:05:58 -0300

    Update vuln_test.py

--- vuln_test.py
+++ vuln_test.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name2__)
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "ok"
+    return "ok1"
 
-if __name2__ == "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
