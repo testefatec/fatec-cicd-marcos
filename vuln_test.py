cf37762d861be2307987d4caa3267a26e3d574b5
Idirley <idirley.soares@fatec.sp.gov.br>
2026-05-24 20:05:14 -0300

    Fix incorrect variable name for Flask app

--- vuln_test.py
+++ vuln_test.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name2__)
 
 @app.route("/")
 def hello():
     return "ok"
 
-if __name__ == "__main__":
+if __name2__ == "__main__":
     app.run(debug=True)
